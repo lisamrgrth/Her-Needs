befd4fb69b7c0a0810f10cfb6480362647aa6044
lisamrgrth <lisatobing1610@gmail.com>
2024-09-08 11:17:56 +0700

    Updating MVT

--- main/models.py
+++ main/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
-    category = models.CharField(max_length=255)
     size = models.CharField(max_length=255)
 
     def __str__(self):
--- main/views.py
+++ main/views.py
@@ -2,14 +2,9 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
+        'app': "herNeeds",
         'name': 'Lisa Margaretha Esron Tobing',
         'class': 'PBP B',
-        'product_name': 'Korean Fashion Cardigan Knitwear',
-        'price': 75000,
-        'description': 'Available in Red',
-        'stock' : '80',
-        'category' : 'Cardigan Wanita',
-        'size': 'Oversize' 
     }
 
     return render(request, "main.html", context)
